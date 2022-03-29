1b17bfb96f5d9f849b77d4b4426efc2f9dc60e57
kolosovpetro <kolosovp94@gmail.com>
2022-03-29 18:49:20 +0200

    code format

--- HotelAccounting/Pages/HomePage.xaml.cs
+++ HotelAccounting/Pages/HomePage.xaml.cs
@@ -46,7 +46,6 @@ namespace HotelAccounting.Pages
             {
                 ListV = new ListView();
             }
-           
 
             var selectedIndex = (RoomFilter)HouseCBox.SelectedIndex;
 
