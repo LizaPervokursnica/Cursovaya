69396f164ff2be5a881f8a6b7d4505aeb6adbe4c
kolosovpetro <kolosovp94@gmail.com>
2022-03-29 18:49:06 +0200

    hopefully fixed

--- HotelAccounting/Pages/HomePage.xaml.cs
+++ HotelAccounting/Pages/HomePage.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace HotelAccounting.Pages
 {
@@ -41,39 +42,44 @@ namespace HotelAccounting.Pages
 
         private void HouseCBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            ListV = new ListView();
+            if (ListV == null)
+            {
+                ListV = new ListView();
+            }
+           
+
             var selectedIndex = (RoomFilter)HouseCBox.SelectedIndex;
 
             if (selectedIndex == RoomFilter.Free)
             {
+                ListV.ItemsSource = null;
                 var freeList = context.Rooms.Where(x => !x.GuestId.HasValue).ToList();
                 var freeObservableCollection = new ObservableCollection<Room>();
                 ListV.ItemsSource = freeObservableCollection;
 
                 freeList.ForEach(x => freeObservableCollection.Add(x));
-                ListV.Items.Refresh();
 
                 return;
             }
 
             if (selectedIndex == RoomFilter.Occupied)
             {
+                ListV.ItemsSource = null;
                 var occupiedList = context.Rooms.Where(x => x.GuestId.HasValue).ToList();
                 var occupiedObservableCollection = new ObservableCollection<Room>();
                 ListV.ItemsSource = occupiedObservableCollection;
 
                 occupiedList.ForEach(x => occupiedObservableCollection.Add(x));
-                ListV.Items.Refresh();
 
                 return;
             }
 
+            ListV.ItemsSource = null;
             var list = context.Rooms.ToList();
             var observableCollection = new ObservableCollection<Room>();
             ListV.ItemsSource = observableCollection;
 
             list.ForEach(x => observableCollection.Add(x));
-            ListV.Items.Refresh();
         }
 
         private void SearchBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
