471d8d6584990615b42f5bfbbfe2a1ed87da509c
Bebranutaya Sova <rjurt122@yandex.ru>
2022-05-09 11:20:33 +0300

    -

--- HotelAccounting/Classes/CheckLink.cs
+++ HotelAccounting/Classes/CheckLink.cs
@@ -3,5 +3,5 @@
 public class CheckLink
 {
     public static string CheckCorrectLink(string link) =>
-       link.Length > 8 && (link.Substring(0, 8) == "https://" || link.Substring(0, 7) == "http://") ? link : "https://www.kindpng.com/picc/m/52-526072_unknown-character-hd-png-download.png";
+       link.Length > 8 && link.StartsWith("https://") || link.StartsWith("http://") ? link : "https://www.kindpng.com/picc/m/52-526072_unknown-character-hd-png-download.png";
 }
